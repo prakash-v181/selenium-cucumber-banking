18289de725c55391ac20161414cd03ee94afbcca
prakash-v181 <prakashtare110@outlook.com>
2026-01-05 17:00:05 +0530

    the fix two

--- src/test/java/TestComponents/HooksClass.java
+++ src/test/java/TestComponents/HooksClass.java
@@ -7,37 +7,27 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
 import java.time.Duration;
-import java.util.Properties;
-import java.io.FileInputStream;
 
 public class HooksClass {
 
     private static WebDriver driver;
 
     @Before
-    public void setUp() throws Exception {
-
-        Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream(
-                System.getProperty("user.dir") + "/src/main/java/Resources/Global.properties"
-        );
-        prop.load(fis);
-
-        String headless = prop.getProperty("headless");
-        String url = prop.getProperty("url");
+    public void setUp() {
 
         EdgeOptions options = new EdgeOptions();
 
-        if ("true".equalsIgnoreCase(headless)) {
+        String headless = System.getProperty("headless", "true");
+
+        if (headless.equalsIgnoreCase("true")) {
             options.addArguments("--headless=new");
             options.addArguments("--disable-gpu");
             options.addArguments("--window-size=1920,1080");
         }
 
         driver = new EdgeDriver(options);
-        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(url);
+        driver.get("https://parabank.parasoft.com/parabank/register.htm");
     }
 
     @After
